8d3e29bc98564e683082958ff4ecad3c394765d7
Travis CI User <travis@example.org>
2018-11-09 23:13:53 +0000

    Documentation for carllerche/tokio-serde-json@dc63204

---
+++ tokio_serde_json/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ReadJson","Adapts a stream of JSON encoded buffers to a stream of values by deserializing them."],["WriteJson","Adapts a buffer sink to a value sink by serializing the values as JSON."]]});
